e12db10e50c9a173bc4a81dc9f984dcda5a53320
kanchan-solanke <kanchanssolanke@gmail.com>
2021-07-08 12:52:15 +0530

    line are equal or not is displayed

---
+++ Co-ordinatesequal.java
@@ -0,0 +1,16 @@
+//assume two lines of end points are
+//(x1,x2)(y1,y2)
+
+class Co-ordinatesequal{
+public static void main(String[] args){
+int Len1 = (x2-x1);
+int Len2 = (y2-y1);
+           if(Len1 = Len2){
+System.out.println("two lines are equal");
+}
+            else{
+System.out.println("two lines are not equal");
+}
+}
+
+}
