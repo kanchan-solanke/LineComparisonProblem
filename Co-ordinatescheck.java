45eccf1eb57f4da912af57322beb4e4393f3b5d4
kanchan-solanke <kanchanssolanke@gmail.com>
2021-07-08 13:05:31 +0530

    check end points of line is less, greater or equal

---
+++ Co-ordinatescheck.java
@@ -0,0 +1,21 @@
+
+
+//assume two lines of end points are
+//(x1,x2)(y1,y2)
+
+class Co-ordinatescheck{
+public static void main(String[] args){
+int Len1 = (x2-x1);
+int Len2 = (y2-y1);
+
+if(Len1>Len2){
+Sytem.out.println("length1 is greater than lenghth2");
+}
+else if(Len1=Len2){
+Sytem.out.println("length1 is equal to lenghth2");
+}
+else{
+Sytem.out.println("length is less than lenghth2");
+}
+}
+}
